2feb71bc8a50a24e11bb74c5eccb58d0571a42ed
aya <benaissa.aya@esprit.tn>
2026-04-13 16:27:17 +0100

    delete options for notifications

--- src/main/java/com/eyetwin/interfaces/Community/INotificationService.java
+++ src/main/java/com/eyetwin/interfaces/Community/INotificationService.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface INotificationService {
+
     void createChannelApprovedNotification(Channel channel, int targetUserId) throws SQLException;
     void createChannelRejectedNotification(Channel channel, int targetUserId, String reason) throws SQLException;
 
